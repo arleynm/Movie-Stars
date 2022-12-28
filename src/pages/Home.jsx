9d9885148f279df4c5f3ac95077da782728983b0
arleynm <arleyaugusto5@gmail.com>
2022-12-28 00:31:40 -0300

    Ajuste de rotas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,35 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import MovieCard from "../components/MovieCard";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 const Home =() =>{
-    return(
-        <div>Home</div>
+    const [TopMovies, setTopMovies] = useState([]);
+
+    const getTopRateMovies = async (url) => {
+        const res =await fetch(url);
+        const data = await res.json();
+
+        setTopMovies(data.results);
+    };
+
+    useEffect(() =>{
+        const topRateUrl = `${moviesURL}top_rated?${apiKey}`;
+
+        getTopRateMovies(topRateUrl);
+    }, []);
+
+
+    return (
+        <div className="conatiner">
+            <h2>Melhores Filmes</h2>
+            <div className="movies-conatiner">
+            {TopMovies.length === 0 && <p>Carregando...</p>}
+            {TopMovies.length > 0 && TopMovies.map((movie)=> <MovieCard key={movie.id} movie={movie} />)}
+            </div>
+        </div>
     )
 }
 
